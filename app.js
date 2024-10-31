586fef5dcc0f2e0efe789d91b6aa9a64770b20be
Emauron Garcia Novaes <emauron.novaes@mse.com.br>
2024-10-30 21:06:49 -0300

    feito a tela de login e trabalhando agora no dashboard e nav lateral

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use(express.static('public'));
 
 app.use('/usuarios/login', UserRoute);
